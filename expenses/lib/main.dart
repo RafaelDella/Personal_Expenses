177b95dc8ae0295b0d2e19f7ed7495f6e8f1b596
Rafael Della Giustina <rafael.della64@gmail.com>
2024-09-26 12:22:19 -0300

    organization widgets

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -1,101 +1,38 @@
-import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'components/transaction_user.dart';
 
 main() => runApp(const ExpensesApp());
 
 class ExpensesApp extends StatelessWidget {
-  const ExpensesApp({super.key});
-
+  const ExpensesApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MyHomePage(),
-      debugShowCheckedModeBanner: false,
-    );
+    return const MaterialApp(home: MyHomePage());
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  MyHomePage({super.key});
-
-  final _transctions = [
-    Transaction(
-        id: 't1',
-        title: 'Adidas Samba Verde',
-        value: 245.99,
-        date: DateTime.now()),
-    Transaction(
-        id: 't2', title: 'Shorts JSB', value: 89.99, date: DateTime.now()),
-  ];
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          "Despesas Pessoais",
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 24,
-          ),
-        ),
-        backgroundColor: Colors.blue[600],
+        title: const Text('Despesas Pessoais'),
       ),
       body: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              width: double.infinity,
-              child: const Card(
-                color: Colors.green,
-                child: Text("Graficos"),
-              ),
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: const [
+          SizedBox(
+            child: Card(
+              color: Colors.blue,
+              child: Text('Gráfico'),
+              elevation: 5,
             ),
-            Column(
-              children: _transctions.map((tr) {
-                return Card(
-                  child: Row(
-                    children: [
-                      Container(
-                        margin:
-                            EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.purple,
-                            width: 2,
-                          ),
-                        ),
-                        padding: EdgeInsets.all(10),
-                        child: Text('R\$ ${tr.value.toStringAsFixed(2)}',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 20,
-                                color: const Color.fromARGB(255, 217, 0, 255))),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            tr.title,
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(
-                            tr.date.toString(),
-                            style: TextStyle(
-                                color:
-                                    const Color.fromARGB(255, 119, 112, 112)),
-                          )
-                        ],
-                      ),
-                    ],
-                  ),
-                );
-              }).toList(),
-            )
-          ]),
+          ),
+          TransactionUser(),
+        ],
+      ),
     );
   }
 }
